6818b044f456c781485024e2505d5b098cfdade6
ankan123saha <sahaankan23@gmail.com>
2020-07-24 09:21:13 +0530

    minor change

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(webpackDevMiddleware(compiler, config.devServer));
 app.use(webpackHotMiddleware(compiler));
 app.use(express.static(path.join(__dirname, '../build')));
 
-app.get('/*', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
 
